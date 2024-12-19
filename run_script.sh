93c39f01430f9a5e00219b078dbcfa9bf7f82aa5
shag527 <shagunkatoch527@gmail.com>
2024-12-19 23:44:39 +0530

    added shell script

---
+++ run_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Step 1: Create MySQL Table
+echo "Creating MySQL table..."
+python3 main_script.py create_mysql_table
+
+# Step 2: Crawl Articles, Summarize and store Data in MySQL and Milvus. Also Query Articles
+python3 main_script.py query_articles "Give me the journal those are published last week"
+
+echo "Process completed successfully."
